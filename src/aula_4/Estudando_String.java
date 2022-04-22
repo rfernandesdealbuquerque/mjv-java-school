9d78133b274d8cd9b4baac706bdc5c6c3bbcb6b9
rfd10 <rfd10@duke.edu>
2022-04-22 02:46:26 -0400

    aula4 parte 1 estudo

--- src/aula_4/Estudando_String.java
+++ src/aula_4/Estudando_String.java
@@ -1,13 +1,7 @@
 package aula_4;
 
 public class Estudando_String {
-public static void main(String[] args) {
-String RODRIGO = "RODRIGO";
-int numero = 2;
-if(numero == 2) {
-System.out.println(RODRIGO);
-}
-else
-System.out.println("erro");
-}
+	public static void main(String[] args) {
+	
+	}
 }
