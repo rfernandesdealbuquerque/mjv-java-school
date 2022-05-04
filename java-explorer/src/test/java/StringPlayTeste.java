3dbf5011f288ac2ff59a31736fd3e696cfa51a6f
rfd10 <rfd10@duke.edu>
2022-05-04 02:14:27 -0300

    iniciar pratica com JUnit tests e TDD

---
+++ java-explorer/src/test/java/StringPlayTeste.java
@@ -0,0 +1,7 @@
+
+public class StringPlayTeste { //Criar testes para cada metodo de StringPlay
+							   //Cada teste unitario testa um caso especifico de input do metodo. Ou seja, podemos ter varios testes unitarios
+							   //para um unico metodo, dependendo das funcionalidades e requisitos/expectativas
+							   //e.g. teste unitario para cpf normal e outro teste unitario para cpf com mais de 8 digitos --> requisito/expectativa de funcionamento eh diferente para cada input. 
+
+}
