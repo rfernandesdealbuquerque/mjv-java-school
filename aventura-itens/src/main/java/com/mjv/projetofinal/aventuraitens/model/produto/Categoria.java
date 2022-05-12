715f80b6d0e04d27e93c5ad207505c5206f68594
rfd10 <rfd10@duke.edu>
2022-05-12 17:21:27 -0300

    UML projeto final aventura-itens criado

---
+++ aventura-itens/src/main/java/com/mjv/projetofinal/aventuraitens/model/produto/Categoria.java
@@ -0,0 +1,9 @@
+package com.mjv.projetofinal.aventuraitens.model.produto;
+
+public enum Categoria {
+	MAR,
+	FLORESTA,
+	NEVE,
+	MONTANHA;
+
+}
---
+++ aventura-itens/src/main/java/com/mjv/projetofinal/aventuraitens/model/produto/Produto.java
@@ -0,0 +1,13 @@
+package com.mjv.projetofinal.aventuraitens.model.produto;
+
+public class Produto {
+	private Integer id;
+	private String descricao;
+	private Double precoVenda;
+	private Integer quantidadeEmEstoque;
+	private Categoria categoria;
+	private Aventura aventura;
+	private Disponibilidade disponibilidade;
+	
+
+}
